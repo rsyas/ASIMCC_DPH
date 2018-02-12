ee4066f35c68ed0da0a9b44b0f66a4672093e634
rsyas <30878348+rsyas@users.noreply.github.com>
2018-02-12 15:30:33 -0500

    ASIMCC Event Log installer

---
+++ AEventLogInstaller.cs
@@ -0,0 +1,44 @@
+﻿/*******************************************************************************/
+/* Class        :   AEventLogInstaller                                         */
+/* Version      :   1.00                                                       */
+/*                                                                             */
+/* Created      :   17-Jan-2018                                                */
+/* Author       :   Reginald Syas                                              */
+/*                                                                             */
+/* Description  :   Create Event Source(s) in registry; this is required       */
+/*                  befoe attempting to write to the Windows event log         */
+/*                  "ASIMCC"                                                   */
+/*                                                                             */
+/*******************************************************************************/
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace Asimcc.Integration.aEventLogInstaller
+{
+    [RunInstaller(true)]
+    public class AEventLogInstaller : Installer
+    {
+        public AEventLogInstaller()
+        {
+            // Log Name that source is created in
+            // ASIMCC log file is called ASIMCClog with the following sources
+            string _logName = "SendSSHIELog";
+            InstallLogSource(_logName, "SendSSHIE");
+            InstallLogSource(_logName, "CompressFile");
+            InstallLogSource(_logName, "DecompressFile");
+        }
+
+        private void InstallLogSource(string logName, string sourceName)
+        {
+            EventLogInstaller aEventLogInstaller = new EventLogInstaller();  //Create Instance of EventLogInstaller
+            aEventLogInstaller.Log = logName;                                // Log Name that source is created in
+
+            if (!System.Diagnostics.EventLog.SourceExists(sourceName))
+            {
+                aEventLogInstaller.Source = sourceName;                      // Set the Source of Event Log, to be created. 
+                Installers.Add(aEventLogInstaller);                          // Add EDIEventLogInstaller to the Installers Collection.
+            }           
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,28 @@
+﻿Purpose:
+When the Web Service attempts to make an entry in the log, it encounters the System.Security.SecurityException.
+It complains about not being able to find the Event Source.  It fails to create the Event Source.
+Because by default ASPNET/Network Services does not have the correct user rights to create an event source.
+
+To solve this problem:
+Using the EventLogInstaller class in the System.Diagnostics namespace create an Event source.
+This Dll "SendSSHIE.Integration.aEventLogInstaller.dll" is used to create Event Sources used by the WebSite1 Service windows event viewer log -SendSSHIE.
+
+How to Do:
+Open command prompt - 'Run as administrator'.
+
+Change to the directory where the "SendSSHIE.Integration.aEventLogInstaller.dll" is located.
+Then execute the following command:
+
+InstallUtil SendSSHIE.Integration.EventLogInstaller.dll
+
+InstallUtil displays the progress of creating a new Event Sources.
+It also logs the progress in the SendSSHIE.Integration.EventLogInstaller.InstallLog file 
+that is created in the same location as the SendSSHIE.Integration.aEventLogInstaller.dll.
+After the commit phase, it displays the message:
+
+The Commit phase completed successfully.
+The transacted install has completed.
+
+Now that the Event Source is successfully created. we can find it in Registry Editor.
+
+Registry key path = HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\eventlog\SendSSHIE
